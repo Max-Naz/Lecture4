369aee190e84706778da9eea8b91407e30848c7a
Maxim Nazarenko <maxim.nazarenko@protonmail.com>
2018-11-16 12:15:34 +0200

    initial project version

---
+++ src/test/java/com/qatestlab/DriverManager.java
@@ -0,0 +1,30 @@
+package com.qatestlab;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+import java.io.File;
+
+public class DriverManager {
+
+
+    public  static WebDriver getDriver(String browser) {
+        switch (browser) {
+            case "firefox":
+                System.setProperty("webdriver.gecko.driver",
+                        new File(DriverManager.class.getResource("/geckodriver_0_23_0.exe").getFile()).getPath());
+                return new FirefoxDriver();
+            case "ie":
+                System.setProperty("webdriver.ie.driver",
+                        new File(DriverManager.class.getResource("/IEDriverServer_3_14_0.exe").getFile()).getPath());
+                return new InternetExplorerDriver();
+            case "chrome":
+            default:
+                System.setProperty("webdriver.chrome.driver",
+                        new File(DriverManager.class.getResource("/chromedriver_2_43.exe").getFile()).getPath());
+                return new ChromeDriver();
+        }
+    }
+}
